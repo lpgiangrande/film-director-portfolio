083e392166b34fb5fe3bcb7f7fe5999d0c215375
lpgiangrande <lauragiangrande@protonmail.com>
2022-09-18 05:41:55 +0200

    external script for loader

---
+++ public/js/loader.js
@@ -0,0 +1,19 @@
+/*
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
+*/
+
+
+
+
+
+// how to create a route in expressjs ?
+
+
