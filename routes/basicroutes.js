878443269b2f3ae218f5e0c8f4b4b2416df92f9f
lpgiangrande <lauragiangrande@protonmail.com>
2022-09-18 05:37:56 +0200

    basic routes

--- routes/basicroutes.js
+++ routes/basicroutes.js
@@ -1,59 +1,23 @@
-/* Routes for the 4 main pages */
+/* Routes for the 4 main pages - GET */
 
 const express = require("express");
 const router = express.Router();
-const Thumbnail = require('../models/modelsThumbnails');
 const ejs = require('ejs');
+const mainController = require('../controllers/main_controller');
 
 
 // HOME PAGE
-router.get('/', (req, res) => {
-    Thumbnail.find({}, function(err, thumbnails){
-        res.render('index', {thumbnailsList: thumbnails })
-    })  
-});
+router.get('/', mainController.homePage);
 
 // PAGE 2 ANIMATION : only display thumbnails with category:animation
-router.get('/animation', (req, res) => {
-    const query = Thumbnail.find({ 'category': 'animation' })
-    query.exec(function(err, thumbnails){
-        res.render('animation', {
-            thumbnailsList: thumbnails
-        })
-    })     
-});
+router.get('/animation/', mainController.animationPage);
 
 // PAGE 3 LIVE ACTION : only display thumbnails with category: live action
-router.get('/liveaction', (req, res) => {
-    const query = Thumbnail.find({ 'category': 'live action' })
-    query.exec(function(err, thumbnails){
-        res.render('liveaction', {
-            thumbnailsList: thumbnails
-        })
-    })   
-});
-
+router.get('/liveaction/', mainController.liveActionPage);
 // PAGE 4 ABOUT
-router.get('/about', (req, res) => {
-    res.render('about')
-});
-
+router.get('/about', mainController.aboutPage);
 
 // Display project details when you clic on a homepage thumbnail
-router.get('/:id', (req, res) => {
-    Thumbnail.findById(req.params.id)
-    .exec()
-    .then(thumbnails => {
-        res.render('project', {
-            thumbnailsList: thumbnails
-        })
-    })
-    .catch(error =>{
-        console.log(error);
-    })   
-})
-
+router.get('/:id', mainController.projectFromHomePage);
 
 module.exports = router;
-
-
