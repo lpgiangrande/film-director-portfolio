f43ef497be5ecb0117930a2f3dd9a3747e443352
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-04 13:01:39 +0100

    code alternatif

--- dbConnect.js
+++ dbConnect.js
@@ -10,3 +10,32 @@ dbConnect = mongoose.connect(process.env.MONGODB_URI,
 
 
   module.exports = dbConnect;
+
+
+
+/* https://www.freecodecamp.org/news/introduction-to-mongoose-for-mongodb-d2a7aa593c57/
+
+
+let mongoose = require('mongoose');
+
+const server = '127.0.0.1:27017'; // REPLACE WITH YOUR DB SERVER
+const database = 'fcc-Mail';      // REPLACE WITH YOUR DB NAME
+
+class Database {
+  constructor() {
+    this._connect()
+  }
+  
+_connect() {
+     mongoose.connect(`mongodb://${server}/${database}`)
+       .then(() => {
+         console.log('Database connection successful')
+       })
+       .catch(err => {
+         console.error('Database connection error')
+       })
+  }
+}
+
+module.exports = new Database()
+  */
