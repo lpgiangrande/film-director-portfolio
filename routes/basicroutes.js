22b0a8c7923d348ed2e4ba5a295f73d838bf2de3
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-07 06:45:59 +0100

    commentaires inutiles enlevés

--- routes/basicroutes.js
+++ routes/basicroutes.js
@@ -1,26 +1,15 @@
-/* Routes for the 4 main pages - GET */
+/* GET Routes for main pages (user) */
 
 const express = require("express");
 const router = express.Router();
 const mainController = require('../controllers/main_controller');
 
 
-
-// HOME PAGE
 router.get('/', mainController.homePage);
-
-// PAGE 2 ANIMATION : only display thumbnails with category:animation
 router.get('/animation/', mainController.animationPage);
-
-// PAGE 3 LIVE ACTION : only display thumbnails with category: live action
 router.get('/liveaction/', mainController.liveActionPage);
-
-// PAGE 4 ABOUT
 router.get('/about', mainController.aboutPage);
-
-// Display project details when you clic on a homepage thumbnail
+// Display project details when clic on a homepage/animation page/liveaction page thumbnail
 router.get('/:id', mainController.seeFullProject);
-
-
 
 module.exports = router;
