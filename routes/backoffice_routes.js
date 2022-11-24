6707531dbf4bb872e227bb72b971765105005bd6
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-24 18:34:35 +0100

    .

--- routes/backoffice_routes.js
+++ routes/backoffice_routes.js
@@ -5,7 +5,64 @@ const backofficeController = require('../controllers/backoffice_controller');
 const thumbnailsSchema = require('../models/modelsThumbnails');
 const projectSchema = require('../models/modelsProject');
 
-require('dotenv').config();
+
+/* ADMIN ROUTES */
+
+
+// GET
+
+// Log In page 
+router.get('/', (req, res) => { // '/admin 
+  res.render('auth');
+})
+
+// projects list page
+router.get('/list', backofficeController.list);
+
+// update thumbnail - update project
+router.get('/updateThumbnail', (req, res) => {
+res.render('updateThumbnail');
+})
+router.get('/updateProject', (req, res) => {
+res.render('updateProject');
+})
+
+// Add thumbnail page (thumbnails image/video seen on Homepage)
+router.get('/uploadThumbnail', (req, res) => {
+  res.render('uploadThumbnail');
+})
+
+// Add project page 
+router.get('/uploadProject', (req, res) => {
+  thumbnailsSchema.find()
+  .populate("project")
+  .exec()
+  .then(thumbnails => {
+      res.render('uploadProject', {thumbnailsList: thumbnails})
+    })
+    .catch();
+})
+
+// POST
+
+// Upload files paths to the database (homepage - thumbnails) 
+router.post('/uploadThumbnail', 
+/*upload.fields(
+  [
+    { name: 'img_thumbnail', maxCount: 1}, 
+    { name: 'vid_thumbnail', maxCount: 1}
+  ]
+),*/ backofficeController.addThumbnail);
+
+// POST route for uploading text and files paths to the database for the Project page 
+router.post('/uploadProject', backofficeController.addProject);     
+
+
+module.exports = router;
+
+
+
+//require('dotenv').config();
 //const multer = require('multer');
 
 /* SIMPLE MULTER CONFIG
@@ -31,59 +88,7 @@ const upload = multer({ storage: storage })*/
 
 
 
-/* ADMIN ROUTES */
 
-
-// GET
-
-// Log In page 
-router.get('/', (req, res) => { // '/admin 
-    res.render('auth');
-})
-
-// projects list page
-router.get('/list', backofficeController.list);
-
-// update thumbnail - update project
-router.get('/updateThumbnail', (req, res) => {
-  res.render('updateThumbnail');
-})
-router.get('/updateProject', (req, res) => {
-  res.render('updateProject');
-})
-
-// Add thumbnail page (thumbnails image/video seen on Homepage)
-router.get('/uploadThumbnail', (req, res) => {
-    res.render('uploadThumbnail');
-})
-
-// Add project page 
-router.get('/uploadProject', (req, res) => {
-    thumbnailsSchema.find()
-    .populate("project")
-    .exec()
-    .then(thumbnails => {
-        res.render('uploadProject', {thumbnailsList: thumbnails})
-      })
-      .catch();
-})
-
-// POST
-
-// Upload files paths to the database (homepage - thumbnails) 
-router.post('/uploadThumbnail', 
-  /*upload.fields(
-    [
-      { name: 'img_thumbnail', maxCount: 1}, 
-      { name: 'vid_thumbnail', maxCount: 1}
-    ]
-  ),*/ backofficeController.addThumbnail);
-
-// POST route for uploading text and files paths to the database for the Project page 
-router.post('/uploadProject', backofficeController.addProject);     
-
-
-module.exports = router;
 
 
 
