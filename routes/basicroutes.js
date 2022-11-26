7f3a5c9349d12a04392a64a575a02b7067dcaf2f
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-26 11:12:08 +0100

    removed a secret route

--- routes/basicroutes.js
+++ routes/basicroutes.js
@@ -22,13 +22,12 @@ router.post('/login', (req, res, next) => {
   });
 
 
-//router.post('/register', mainController.handleRegistration); 
+
     
 router.get('/', mainController.homePage); 
 router.get('/animation/', mainController.animationPage);
 router.get('/liveaction/', mainController.liveActionPage);
 router.get('/about', mainController.aboutPage);
-//router.get('/register', forwardAuthenticated, mainController.registerPage);
 router.get('/login',  forwardAuthenticated, mainController.loginPage);
 // Display project details when clic on a homepage/animation page/liveaction page thumbnail
 router.get('/:id', mainController.seeFullProject);
