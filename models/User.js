5c1c8473e0206be8b2c47a58ca03da375ae13afb
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-27 02:58:17 +0100

    remove comments

--- models/User.js
+++ models/User.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs');
-
 
 // User
 
@@ -20,33 +18,5 @@ const userSchema = mongoose.Schema({
     }
 });
 
-/*
-userSchema.pre('save', async function(next) {
-    try {
-        // check method of registration
-        const user = this;
-        if (!user.isModified('password')) next();
-        // generate salt
-        const salt = await bcrypt.genSalt(10);
-        // hash the password
-        const hashedPassword = await bcrypt.hash(this.password, salt);
-        // replace plain text password with hashed password
-        this.password = hashedPassword;
-        next();
-        } 
-    catch (error) {
-        return next(error);
-    }
-});
-
-userSchema.methods.matchPassword = async function (pwd) {
-    try {
-        return await bcrypt.compare(pwd, this.pwd);
-        } 
-    catch (error) {
-        throw new Error(error);
-    }
-};
-*/
 
 module.exports = mongoose.model('User', userSchema);
