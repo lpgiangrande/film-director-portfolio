3acfee1e80f84d7df73e83b308167b9298eeccf7
Laura Giangrande <lauragiangrande@protonmail.com>
2022-11-28 06:53:34 +0100

    helmet

--- server.js
+++ server.js
@@ -41,6 +41,11 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 
 //app.use(cors())
 
+// Helmet (helps secure your Express apps by setting various HTTP headers)
+// -> doc : https://helmetjs.github.io/
+app.use(helmet());
+app.disable('x-powered-by');
+
 // Session Configuration 
 app.use(
   session({
