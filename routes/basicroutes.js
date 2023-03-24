420cde4a8ab50de2aeae71fe6dd45351f1ba9cbe
Laura G <lauragiangrande@protonmail.com>
2023-03-24 08:46:02 +0100

    jsdoc

--- routes/basicroutes.js
+++ routes/basicroutes.js
@@ -1,4 +1,8 @@
-/* GET Routes for main pages (user) */
+/**
+ * GET routes for the user
+ * GET route for the client to login
+ * GET / POST route once used to create 2 CMS users (me and client, limit of 2 accounts)
+ */
 
 const express = require("express");
 const router = express.Router();
@@ -11,23 +15,33 @@ const { forwardAuthenticated } = require('../config/auth');
 
 
 /**
- * auth | logged in
+ * POST route for user login.
+ *
+ * @function
+ * @name login
+ * @memberof router
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {function} next - The next middleware function.
+ * @returns {undefined}
  */
 router.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-      successRedirect: '/admin/list',
-      failureRedirect: '/login',
-      failureFlash: true
-    })(req, res, next);
-  });
+  passport.authenticate('local', {
+    successRedirect: '/admin/list',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
+});
 
-    
-router.get('/', mainController.homePage); 
+
+router.post('/register', mainController.handleRegistration);
+
+router.get('/', mainController.homePage);
 router.get('/animation/', mainController.animationPage);
 router.get('/liveaction/', mainController.liveActionPage);
 router.get('/about', mainController.aboutPage);
-router.get('/login',  forwardAuthenticated, mainController.loginPage);
-// Display project details when clic on a homepage/animation page/liveaction page thumbnail
+router.get('/register', forwardAuthenticated, mainController.registerPage);
+router.get('/login', forwardAuthenticated, mainController.loginPage);
 router.get('/:id', mainController.seeFullProject);
 
 
