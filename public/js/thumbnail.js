d63074042e35c915e103f76241912683a32cc8f2
Laura G <lauragiangrande@protonmail.com>
2023-05-16 01:30:17 +0200

    clean indent and code order

--- public/js/thumbnail.js
+++ public/js/thumbnail.js
@@ -6,6 +6,7 @@ This js file has two purposes :
 
 - 1. Make thumbnails title appear when mouse enter
 - 2. Switch thumbnail content from img to video when mouse enter
+- 3. On mouseleave (default) = show only thumbnail image
 
 */
 'use strict';
@@ -17,29 +18,30 @@ const thumbnail_vid = document.querySelectorAll("video.thumbnail_vid");
 
 
 
-for (let i = 0 ; i < mouseTarget.length && i < targetTitle.length && i < thumbnail_img.length && i < thumbnail_vid.length; i++) {
+for (let i = 0 ; 
+    i < mouseTarget.length 
+    && i < targetTitle.length 
+    && i < thumbnail_img.length 
+    && i < thumbnail_vid.length; i++) 
+    {
 
-    // OVER
-    mouseTarget[i].addEventListener('mouseover', show); 
-    
-    function show(){
+        // OVER
+        mouseTarget[i].addEventListener('mouseover', show); 
+        // LEAVE
+        mouseTarget[i].addEventListener('mouseleave', hide);
 
-        thumbnail_vid[i].style.opacity = "0.5";
-        targetTitle[i].style.opacity = "1"; 
-        thumbnail_vid[i].style.display = "block";
-        thumbnail_img[i].style.display = "none";
+        function show(){
+            thumbnail_vid[i].style.opacity = "0.5";
+            targetTitle[i].style.opacity = "1"; 
+            thumbnail_vid[i].style.display = "block";
+            thumbnail_img[i].style.display = "none";
+        }
+
+        function hide(){
+            thumbnail_img[i].style.opacity = "1";
+            targetTitle[i].style.opacity = "0"; 
+            thumbnail_vid[i].style.display = "none";
+            thumbnail_img[i].style.display = "block";
+        }
     }
-
-    // LEAVE
-    mouseTarget[i].addEventListener('mouseleave', hide);
-
-    function hide(){
-        thumbnail_img[i].style.opacity = "1";
-        targetTitle[i].style.opacity = "0"; 
-        thumbnail_vid[i].style.display = "none";
-        thumbnail_img[i].style.display = "block";
-    }
-
- }
-
 
