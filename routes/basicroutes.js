32113edd18ec2b295daf31a9144112e254cd7db2
Laura G <lauragiangrande@protonmail.com>
2023-05-16 15:39:42 +0200

    jsdoc update for the login post route

--- routes/basicroutes.js
+++ routes/basicroutes.js
@@ -12,17 +12,37 @@ const { forwardAuthenticated } = require('../config/auth');
 const rateLimit = require('express-rate-limit');
 const he = require('he'); // prevent cross-site scripting (XSS) attacks by encoding the characters
 
-// Protect auth form with rateLimit middleware
+/**
+ * POST route for user login.
+ *
+ * @function
+ * @name login
+ * @memberof router
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {function} next - The next middleware function.
+ * @returns {undefined}
+ */
+
+/**
+ * Rate limiting configuration object for login requests.
+ *
+ * @typedef {Object} LimiterConfig
+ * @property {number} windowMs - The duration in milliseconds for which the login attempts are counted.
+ * @property {number} max - The maximum number of allowed login attempts within the specified duration.
+ * @property {boolean} standardHeaders - Indicates whether to include standard rate limiting headers in the response.
+ * @property {boolean} legacyHeaders - Indicates whether to include legacy rate limiting headers in the response.
+ * @property {string} message - The message to be displayed when the rate limit is exceeded.
+ */
+
 const limiter = rateLimit({
   //windowMs: 1000, // Set a very short ban duration (1 second) for testing purposes
   windowMs: 15 * 60 * 1000, // ban 15 minutes + fail2ban 
-	max: 3, 
-	standardHeaders: true, 
-	legacyHeaders: false, 
+  max: 3, 
+  standardHeaders: true, 
+  legacyHeaders: false, 
   message: "Too many login attempts from this IP, please try again in 15 minutes",
-})
-
-
+});
 
 /**
  * POST route for user login.
@@ -43,8 +63,8 @@ router.post('/login', limiter, (req, res, next) => {
   // Escape characters that may cause security issues
   const sanitizedUsername = he.encode(username);
   const sanitizedPwd = he.encode(pwd);
-  console.log('Sanitized username:', sanitizedUsername);
-  console.log('Sanitized pwd:', sanitizedPwd);
+  // console.log('Sanitized username:', sanitizedUsername);
+  // console.log('Sanitized pwd:', sanitizedPwd);
 
   passport.authenticate('local', {
     successRedirect: '/admin/list',
