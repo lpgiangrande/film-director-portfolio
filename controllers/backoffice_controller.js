e698826344ddfb3c97f1ab2edcf6957d81ef4fed
Laura G <lauragiangrande@protonmail.com>
2023-05-17 20:39:10 +0200

    async await syntax

--- controllers/backoffice_controller.js
+++ controllers/backoffice_controller.js
@@ -1,126 +1,83 @@
 const mongoose = require("mongoose");
-const thumbnailsSchema = require('../models/Thumbnails');
-const projectSchema = require('../models/Project');
+const Thumbnail = require('../models/Thumbnails');
+const Project = require('../models/Project');
 require('../config/passport') // test
 
 /**
- * addProject from admin | upload
+ * addProject from admin | upload 
+ * Each "project" is a new web page with project id, title, images & video links...
+ * & a thumbnail added on the homepage to clic on it 
  * @param {*} req  
  * @param {*} res 
- * Convert videos input content into an array or videos urls
- * Map over req.files to get array of images
+ * 
+ * Array of trimmed video links obtained from the request body.
+ * @const {Array<string>} array_vids 
+ * 
+ * Array of trimmed images links obtained from the request body
+ * @const {Array<string>} gallery
+ * 
+ * Project object representing a new project.
+ * @const {newProject} project
+ * @properties ... id, thumbnail, title, ... gallery ...
  */
-
-
-/* ONLY IF VIDS && IMG ARE UPLOADED FILES :
-
-
-let vids = []; 
-
-for(let i = 0; i < req.files.length; i++){ 
-
-    vids[i] = req.files[i].path; 
-    vids = vids.filter(vid => vid.endsWith('.mp4'))
-} 
-
-let  visuals = []; 
-
-for(let j = 0; j < req.files.length; j++){ 
-
-    visuals[j] = req.files[j].path;
-    visuals = visuals.filter(visual => visual.endsWith('.jpg'))
-    
-}
-
-*/
 
 exports.addProject = (req, res) => {
 
-    // vids array
-    let array_vids = [];
-    let contentFromInput = req.body.array_vids;
-    let splitContent = contentFromInput.split(',');
+    const array_vids = req.body.array_vids.split(',').map(vid => vid.trim());
+    const gallery = req.body.gallery.split(',').map(gallery_img => gallery_img.trim());
 
-    splitContent.forEach(function(vid){
-        array_vids.push(vid.trim());
-    });
-    
-    // imgs array - if file upload : 
-    // let array_files = req.files;
-    // let visuals = array_files.map(visual => visual.path);
-
-    // imgs array - if upload aws img links : 
-    let gallery = [];
-    let contentFromInput2 = req.body.gallery;
-    let splitContent2 = contentFromInput2.split(',');
-
-    splitContent2.forEach(function(gallery_img){
-        gallery.push(gallery_img.trim());
-    });
-
-
-    const project = new projectSchema({
-
+    const newProject = new Project({
         _id: new mongoose.Types.ObjectId(),
-
         thumbnail : req.body.linkedThumbnail,
-        //category : req.body.category,
         project_title : req.body.project_title,
         director : req.body.director,
         other_contributors : req.body.other_contributors,
         productor : req.body.productor,
-
-        // Videos
-        array_vids : array_vids, 
+        array_vids : array_vids, // ARRAY
         video2_description: req.body.video2_description, 
         video3_description: req.body.video3_description,
         video4_description: req.body.video4_description,
         video5_description: req.body.video5_description,
         video6_description: req.body.video6_description,
         video7_description: req.body.video7_description,
-
-        //images
-        gallery : gallery, // gallery_video : req.files.gallery_video;
+        gallery : gallery, // ARRAY
         gallery_row_1_description : req.body.description_1,
         gallery_row_2_description : req.body.description_2,
         gallery_row_3_description : req.body.description_3,
         gallery_row_4_description : req.body.description_4
+    });
 
-    })
-    
-    project.save()
+    newProject
+        .save()
+        .then(result => {
+            console.log(result);
+            res.redirect(301, '/admin/list');
+        })
+        .catch(error => {
+            console.log(error);
+        });
 
-    .then(result => {
-        console.log(result);
-        res.redirect(301, '/admin/list');
-
-    })
-    .catch(error => {
-        console.log(error);
-    })
-}
+};
 
 /**
- * addThumbnail from admin 
+ * addThumbnail from admin - before adding a project page, add its thumbnail for the home page
  * @param {*} req 
  * @param {*} res 
  */
 
 exports.addThumbnail = (req, res) => {
 
-    const thumbnail = new thumbnailsSchema({
-
+    const newThumbnail = new Thumbnail({
         _id: new mongoose.Types.ObjectId(),
         title : req.body.title_thumbnail,
         category : req.body.category,
         imgSrc : req.body.img_thumbnail, //req.files.img_thumbnail[0].path, 
         videoSrc : req.body.vid_thumbnail, //req.files.vid_thumbnail[0].path, 
         releaseDate : req.body.release_date
+    });
 
-    })
-
-    thumbnail.save()
-
+    newThumbnail
+        .save()
         .then(result => {
             console.log("result = ",result);
             res.redirect(301, '/admin/uploadProject');
@@ -129,8 +86,7 @@ exports.addThumbnail = (req, res) => {
         .catch(error => {
             console.log(error);
             res.status(500).json({msg: 'Error'});
-        })
-
+        });
 }
 
 
@@ -139,127 +95,117 @@ exports.addThumbnail = (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-exports.list = (req, res) => {
-    projectSchema.find({}, function(err, projects){
-        thumbnailsSchema.find({}, function(err,thumbnails) {
-            res.render('list', {
-                projectsList: projects,
-                thumbnailsList: thumbnails,
-                username: req.user.username
-            })
-        })
-    })
-    .catch(error => {
+exports.list = async (req, res) => {
+
+    try {
+      const projects = await Project.find({});
+      const thumbnails = await Thumbnail.find({});
+  
+      res.render('list', {
+        projectsList: projects,
+        thumbnailsList: thumbnails,
+        username: req.user.username,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+  
+
+// UPDATE thumbnail by id
+exports.updateThumbnail = async (req, res) => {
+
+    try {
+        const id = req.params.id;
+        const thumbnail = await Thumbnail.findById(id).exec();
+
+        res.render('updateThumbnail', {
+            thumbnail: thumbnail
+        });
+    } catch (error) {
         console.log(error);
-    })
+    }
 
-}
+};
 
-// UPDATE thumbnail by id 
-exports.updateThumbnail = (req, res) => {
-
-    const id = req.params.id;
-
-    thumbnailsSchema.findById(id)
-    //.then(thumbnail => res.json(thumbnail))
-    //.catch(err => res.status(400).json('The fuck is the Error: ' + err));
-        .exec()
-        .then(thumbnail => {
-            res.render("updateThumbnail", {
-                thumbnail: thumbnail
-            })
-        })
-}
 
 // UPDATE project by id
-exports.updateProject = (req, res) => {
+exports.updateProject = async (req, res) => {
 
-    const projectId = req.params.id;
+    try {
+        const projectId = req.params.id;
 
-    thumbnailsSchema.find()
-    .exec()
-    .then(projects => {
-        projectSchema.findById(projectId)
-        .populate("thumbnail")
-        .exec()
-        .then(project => {
-            res.render("updateProject", {
-                project: project
-            })
-        })
-    })
-}
+        await Thumbnail.find().exec();
+        const project = await Project.findById(projectId).populate("thumbnail").exec();
 
-
-exports.handleThumbnailUpdate = (req, res) => {
-
-    const thumbnailUpdate = {
-        title : req.body.title_thumbnail,
-        category : req.body.category,
-        imgSrc : req.body.img_thumbnail, 
-        videoSrc : req.body.vid_thumbnail
-    }
-    thumbnailsSchema.updateOne({_id: req.body.identifiant}, thumbnailUpdate)
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.redirect(301, '/admin/list');
-    })
-    .catch(error => {
+        res.render("updateProject", {
+        project: project
+        });
+    } catch (error) {
         console.log(error);
-    })
-    
-}
+    }
 
-exports.handleProjectUpdate = (req, res) => {
-
-    let array_vids = [];
-    let contentFromInput = req.body.array_vids;
-    let splitContent = contentFromInput.split(',');
-
-    splitContent.forEach(function(vid){
-        array_vids.push(vid.trim());
-    });
+};
   
-    let gallery = [];
-    let contentFromInput2 = req.body.gallery;
-    let splitContent2 = contentFromInput2.split(',');
 
-    splitContent2.forEach(function(gallery_img){
-        gallery.push(gallery_img.trim());
-    });
+exports.handleThumbnailUpdate = async (req, res) => {
 
-    const projectUpdate = {
-        thumbnail : req.body.linkedThumbnail,
-        project_title : req.body.project_title,
-        director : req.body.director,
-        other_contributors : req.body.other_contributors,
-        productor : req.body.productor,
+    try {
+      const thumbnailUpdate = {
+        title: req.body.title_thumbnail,
+        category: req.body.category,
+        imgSrc: req.body.img_thumbnail,
+        videoSrc: req.body.vid_thumbnail
+      };
+  
+      await Thumbnail.updateOne({ _id: req.body.identifiant }, thumbnailUpdate).exec();
+  
+      //console.log(result);
+      res.redirect(301, '/admin/list');
+    } catch (error) {
+      console.log(error);
+    }
 
-        array_vids : array_vids, 
-        video2_description: req.body.video2_description, 
+  };
+  
+
+exports.handleProjectUpdate = async (req, res) => {
+
+    try {
+      const array_vids = req.body.array_vids.split(',').map(vid => vid.trim());
+      const gallery = req.body.gallery.split(',').map(gallery_img => gallery_img.trim());
+  
+      const projectUpdate = {
+        thumbnail: req.body.linkedThumbnail,
+        project_title: req.body.project_title,
+        director: req.body.director,
+        other_contributors: req.body.other_contributors,
+        productor: req.body.productor,
+  
+        array_vids: array_vids,
+        video2_description: req.body.video2_description,
         video3_description: req.body.video3_description,
         video4_description: req.body.video4_description,
         video5_description: req.body.video5_description,
         video6_description: req.body.video6_description,
         video7_description: req.body.video7_description,
-
-        gallery : gallery, 
-        gallery_row_1_description : req.body.description_1,
-        gallery_row_2_description : req.body.description_2,
-        gallery_row_3_description : req.body.description_3,
-        gallery_row_4_description : req.body.description_4
+  
+        gallery: gallery,
+        gallery_row_1_description: req.body.description_1,
+        gallery_row_2_description: req.body.description_2,
+        gallery_row_3_description: req.body.description_3,
+        gallery_row_4_description: req.body.description_4
+      };
+  
+      const result = await Project.updateOne({ _id: req.body.identifiant }, projectUpdate).exec();
+      //console.log(result);
+      res.redirect(301, '/admin/list');
+    } catch (error) {
+      console.log(error);
     }
-    projectSchema.updateOne({_id: req.body.identifiant}, projectUpdate)
-    .exec()
-    .then(result => {
-        console.log(result);
-        res.redirect(301, '/admin/list');
-    })
-    .catch(error => {
-        console.log(error);
-    })
-    
-}
+};
+  
+  
 
 
