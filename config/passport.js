4991ececf30c05a21d8a4d3968645a44a5a8765a
Laura G <lauragiangrande@protonmail.com>
2023-05-18 15:21:31 +0200

    remvove unused line

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,5 @@
-// const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
-
-// Load User model
 const User = require('../models/User');
 
 module.exports = function(passport) {
