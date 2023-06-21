ee52dbcf9e8e21742982e167338b58acd1d4d7bf
Laura G <lauragiangrande@protonmail.com>
2023-06-21 07:05:40 +0200

    up

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.use(
         "https://ka-f.fontawesome.com",
       ],
       fontSrc: ["'self'", "https://fonts.gstatic.com", "https://ka-f.fontawesome.com"],
-      imgSrc: ["'self'", "https://site-regis.s3.eu-west-3.amazonaws.com"],
+      imgSrc: ["'self'", "https://site-regis.s3.eu-west-3.amazonaws.com",  "https://i.vimeocdn.com"],
       mediaSrc: ["'self'", "https://site-regis.s3.eu-west-3.amazonaws.com"],
       scriptSrcAttr: ["'unsafe-inline'"],
       styleSrcAttr: ["'unsafe-inline'"],
